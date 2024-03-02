b686b3c307d8ee25281114bef7b47cfed4a89a50
邵家驹 <987585547@qq.com>
2024-03-02 20:53:05 +0800

    excel-like ribbon(file tab)

---
+++ RibbonStyle/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Prism.Ioc;
+using Prism.Unity;
+using RibbonStyle.ViewModels;
+using RibbonStyle.Views;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace RibbonStyle
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return     Container.Resolve<ShellWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<ShellWindow, ShellViewModel>();
+        }
+    }
+
+}
---
+++ RibbonStyle/ViewModels/ShellViewModel.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using RibbonStyle.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace RibbonStyle.ViewModels
+{
+    public partial class ShellViewModel : ObservableObject
+    {
+        public List<GridItemStyle> GridItemStyles { get; set; } = new()
+        {
+            new(){Name="常规"},
+            new()
+            {
+                Name="差",
+                Background=new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFFC7CE")),
+                Foreground=new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFB7333B")),
+            },
+             new()
+            {
+                Name="好",
+                Background=new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFC6EFCE")),
+                Foreground=new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF126C00")),
+            }, new()
+            {
+                Name="适中",
+                Background=new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFFEB9C")),
+                Foreground=new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF9C5700")),
+            },
+        };
+    }
+}
