852a8f43c5d3c1097058efa84f5b19a078b287d1
sjj <987585547@qq.com>
2024-03-13 17:30:27 +0800

    datagrid

---
+++ RibbonStyle/Models/TableItem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RibbonStyle.Models
+{
+    public class TableItem
+    {
+        public int Index { get; set; }
+        public string A { get; set; }
+        public string B { get; set; }
+        public string C { get; set; }
+        public string D { get; set; }
+        public string E { get; set; }
+        public string F { get; set; }
+        public string G { get; set; }
+        public string H { get; set; }
+        public string I { get; set; }
+        public string J { get; set; }
+        public string K { get; set; }
+        public string L { get; set; }
+        public string M { get; set; }
+        public string N { get; set; }
+        public string O { get; set; }
+        public string P { get; set; }
+        public string Q { get; set; }
+        public string R { get; set; }
+        public string S { get; set; }
+        public string T { get; set; }
+        public string U { get; set; }
+        public string V { get; set; }
+        public string W { get; set; }
+        public string X { get; set; }
+        public string Y { get; set; }
+        public string Z { get; set; }
+    }
+}
